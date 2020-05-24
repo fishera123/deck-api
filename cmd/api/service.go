ed8a1346953957216f8f82c22042c9949ce8959c
Lasha Tsintsabadze <lasha@entifyme.com>
2020-05-25 00:02:10 +0300

    fixup! feat: createDeck implementation

--- cmd/api/service.go
+++ cmd/api/service.go
@@ -1,10 +1,22 @@
 package api
 
 import (
-	"github.com/fishera123/go-rest-api/pkg/models"
+	"deck-api/pkg/models"
+	"fmt"
 )
 
 func (app *Application) CreateDeck(cardCodes string, isShuffled bool) (models.Deck, error) {
 	cards := createDefaultCardSequence()
 	return app.DeckModel.Create(cards, isShuffled), nil
+}
+
+func createDefaultCardSequence() (codes []string) {
+	for _, suit := range models.SuitsSequence {
+		for _, card := range models.CardsSequence {
+			code := fmt.Sprintf("%s%s", card, suit)
+			codes = append(codes, code)
+		}
+	}
+
+	return codes
 }
